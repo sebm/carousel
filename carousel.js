2ed8557efcab4d95220773ad2038905f0d153a1f
sebm <sebastian.m@gmail.com>
2012-04-08 01:57:38 -0700

    first codes

---
+++ carousel.js
@@ -0,0 +1,12 @@
+(function(window, undefined) {
+  window.Carousel = function() {
+    var initOnId = function(id) {
+      alert(id);
+    };
+
+    return {
+      initOnId : initOnId
+    }
+
+  };
+})(window, undefined);
