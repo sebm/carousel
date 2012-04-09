92cb609dc03a7e3bacff37fd5e6ff590b56cb671
sebm <sebastian.m@gmail.com>
2012-04-08 22:48:57 -0700

    more changes. whooooo

--- carousel.js
+++ carousel.js
@@ -2,6 +2,8 @@
   window.Carousel = function(id) {
     var wrapper = document.createElement('div');
     var outer = document.getElementById(id)
+    var currentPage;
+    var numberPages;
 
     var init = function() {
       initWrapper();
@@ -25,14 +27,44 @@
       outer.appendChild(wrapper);
       wrapper.style.width = elementsWidth + 'px';
       outer.style.overflow = 'hidden';
+
+      currentPage = 1;
+      var outerWidth = parseInt(window.getComputedStyle(outer).width, 10);
+
+      numberPages = Math.ceil(elementsWidth / outerWidth);
     };
 
     var makeButtons = function () {
+      var prevButton = document.createElement('button');
+      var nextButton = document.createElement('button');
 
-    }
+      prevButton.innerHTML = 'prev';
+      nextButton.innerHTML = 'next';
+
+      outer.appendChild(prevButton);
+      outer.appendChild(nextButton);
+
+      prevButton.addEventListener('click', function () {
+        alert('prev!');
+        prevTrigger();
+      });
+
+      nextButton.addEventListener('click', function () {
+        alert('next!');
+        nextTrigger();
+      });
+    };
+
+    var nextTrigger = function() {
+    };
+
+    var prevTrigger = function() {
+    };
 
     return {
-      init: init
+      init: init,
+      prevTrigger: prevTrigger,
+      nextTrigger: nextTrigger
     }
 
   };
