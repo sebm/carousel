e1738392ee281a62fbc3f2f7a5bb846825cfe99c
sebm <sebastian.m@gmail.com>
2012-04-08 19:08:02 -0700

    more progress.

--- carousel.js
+++ carousel.js
@@ -1,11 +1,38 @@
 (function(window, undefined) {
-  window.Carousel = function() {
-    var initOnId = function(id) {
-      alert(id);
+  window.Carousel = function(id) {
+    var wrapper = document.createElement('div');
+    var outer = document.getElementById(id)
+
+    var init = function() {
+      initWrapper();
+      makeButtons();
     };
 
+    var initWrapper = function () {
+
+      wrapper.id = 'imgWrapper'
+
+      var nodes = outer.childNodes;
+      var elementsWidth = 0;
+      for (var i = 0; i < nodes.length; i++) {
+        if (nodes[i].nodeType === Node.ELEMENT_NODE
+          && nodes[i].tagName === 'IMG'
+        ) {
+          elementsWidth += nodes[i].width;
+          wrapper.appendChild(nodes[i]);
+        }
+      }
+      outer.appendChild(wrapper);
+      wrapper.style.width = elementsWidth + 'px';
+      outer.style.overflow = 'hidden';
+    };
+
+    var makeButtons = function () {
+
+    }
+
     return {
-      initOnId : initOnId
+      init: init
     }
 
   };
